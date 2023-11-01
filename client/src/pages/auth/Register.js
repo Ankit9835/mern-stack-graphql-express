e428fee1f785396a2c6e3f950a36720a6b56e692
Ankit9835 <ankydchrismatic@gmail.com>
2023-11-01 23:18:35 +0530

    firebase implementation going on

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
+import { auth } from '../../firebase';
 
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const handleSubmit = () => {
-        //
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            const config = {
+                url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+                handleCodeInApp: true,
+            };
+
+            await auth.sendSignInLinkToEmail(email, config);
+
+            // Store the email in local storage for later use
+            window.localStorage.setItem('emailForRegistration', email);
+
+            // Clear the form and loading state
+            setEmail('');
+            setLoading(false);
+
+            // Display a message or redirect the user to a confirmation page
+            alert('A confirmation email has been sent to your email address. Please check your inbox and follow the instructions to complete the registration.');
+        } catch (error) {
+            console.error('Error sending confirmation email:', error);
+            setLoading(false);
+        }
     };
 
     return (
